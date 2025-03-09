73d4260f863991679edab34a41b5110b3df33506
aruizgg <antoniorzgar@gmail.com>
2025-03-09 11:24:12 +0100

    First commit

---
+++ walleto-back/src/main/java/com/aruizgg/walleto/WalletoApplication.java
@@ -0,0 +1,13 @@
+package com.aruizgg.walleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletoApplication.class, args);
+	}
+
+}
---
+++ walleto-back/src/test/java/com/aruizgg/walleto/WalletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aruizgg.walleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
